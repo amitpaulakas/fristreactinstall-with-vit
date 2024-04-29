b49b48a28592b549a65dbac7ba1767885b608687
amitpaulakas <amitpaulakas@gmail.com>
2024-04-29 11:43:46 +0600

    frist react file open

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Amit paul
+      </p>
+    </>
+  )
+}
+
+export default App
